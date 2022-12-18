838618bb5d32930716c961b7b6043184b4f52620
VipulJadhav12 <vipulsjadhav.12@gmail.com>
2022-12-18 18:00:48 +0530

    Initial commit. Added raw Spring Boot v2.7.6 project.

---
+++ src/main/java/com/afourathon/project_management_rest_api/ProjectManagementRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.afourathon.project_management_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afourathon/project_management_rest_api/ProjectManagementRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afourathon.project_management_rest_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
